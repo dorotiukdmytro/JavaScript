931944fba8483b9de183962ff124b948370c2ed3
Dmytro Dorotiuk <ddorotiuk@gmail.com>
2018-10-07 18:49:11 +0200

    dodano projekt suma-zmiennych

---
+++ suma-zmiennych/js/main.js
@@ -0,0 +1,9 @@
+let liczbaPierwsza = 15;
+let liczbaDruga = 100;
+
+function sumOfNumbers(num1, num2) {
+    sum = num1 + num2;
+    return sum;
+}
+
+console.log(sumOfNumbers(liczbaPierwsza, liczbaDruga));
