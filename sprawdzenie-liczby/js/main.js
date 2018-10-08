f1232c62d4d5a02d0e1ce5466b72205866b338e3
Dmytro Dorotiuk <ddorotiuk@gmail.com>
2018-10-08 21:44:42 +0200

    dodano projekt sprawdzenie-liczby

---
+++ sprawdzenie-liczby/js/main.js
@@ -0,0 +1,9 @@
+function compareNumber(number) {
+    if ((number >= 100) && (number <= 200)) {
+        console.log('Liczba znajduje się w przedziale od 100 do 200');
+    } else {
+        console.log('Liczba nie znajduje się w przedziale od 100 do 200');
+    }
+}
+
+compareNumber(2);
